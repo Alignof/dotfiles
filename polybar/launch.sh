901e04d6d5d0c68441ec4d09f5c9df2afd008710
Alignof <neuromancer_wg@outlook.jp>
2021-06-02 13:52:45 +0900

    [update] support for multi monitors

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -7,6 +7,9 @@ killall -q polybar
 
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar bottom 2>&1 | tee -a /tmp/polybar1.log & disown
+
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+    MONITOR=$m polybar --reload bottom 2>&1 | tee -a /tmp/polybar1.log & disown
+done
 
 echo "Bars launched..."
