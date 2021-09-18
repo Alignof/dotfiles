c1ba381ec6e6557a1ad4bf4a6f84aa2184107f00
Alingof <Alignof@outlook.com>
2021-09-19 00:54:48 +0900

    [update] update the name of default terminal

--- i3/script/load_layout.sh
+++ i3/script/load_layout.sh
@@ -1,4 +1,4 @@
 i3-msg "workspace 1; append_layout /home/takana/.config/i3/workspace-1.json"
-(alacritty -t main_term &)
-(alacritty -o font.size=7 -t with_gotop -e gotop &)
-(alacritty -o font.size=10 -t sub_term &)
+(alacritty -t primary &)
+(alacritty -o font.size=7 -t gotop -e gotop --nvidia &)
+(alacritty -o font.size=10 -t secondary &)
