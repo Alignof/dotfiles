9846af46c8a119a5964386f41ee990990391a823
Alingof <Alignof@outlook.com>
2022-04-14 23:58:38 +0900

    [update] update for 4K

--- i3/script/monitor_detect.sh
+++ i3/script/monitor_detect.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
 #st_prev=$(cat /sys/class/drm/card0-HDMI-A-1/status)
-st_prev="disconnected"
+xrandr --output HDMI-0 --mode 3840x2160 --primary --output DVI-I-1 --mode 1920x1080 --left-of HDMI-0
 
-while :
-do
-    st_next=$(cat /sys/class/drm/card0-DP-2/status)
-    if [ $st_prev != $st_next ]; then
-        st_prev=$st_next
-        if [ $st_next = "connected" ]; then
-	    xrandr --output HDMI-0 --mode 1920x1080 --output DP-2 --mode 1920x1080 
-        else
-	    xrandr --output HDMI-0 --mode 1920x1080 --output DP-2 --off
-        fi
-	~/.fehbg
-    fi
-    sleep 1
-done
+
+# 
+# st_prev="disconnected"
+# 
+# while :
+# do
+#     #st_next=$(cat /sys/class/drm/card0-DP-2/status)
+#     st_next="disconnected"
+#     if [ $st_prev != $st_next ]; then
+#         st_prev=$st_next
+#         if [ $st_next = "connected" ]; then
+# 	    xrandr --output HDMI-0 --mode 3840x2160 --primary --output DVI-I-1 --mode 1920x1080 --right-of HDMI-0
+#         else
+# 	    xrandr --output HDMI-0 --mode 3840x2160 --primary --output DVI-I-1 --off
+#         fi
+# 	~/.fehbg
+#     fi
+#     sleep 1
+# done
+# 
