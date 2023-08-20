423f867876c3f2faff4d211ecb6052df2a5b256e
Alignof <alignof@outlook.com>
2023-08-20 17:15:44 +0900

    [add] add sway/load_layout.sh

--- sway/script/load_layout.sh
+++ sway/script/load_layout.sh
@@ -1,4 +1,10 @@
-swaymsg "workspace 7; append_layout /home/takana/.config/i3/workspace-7.json"
-(alacritty -t primary &)
-(alacritty -o font.size=7 -t bottom -e btm --enable_gpu_memory --hide_table_gap &)
-(alacritty -o font.size=10 -t secondary &)
+#!/bin/zsh
+swaymsg "workspace 7;
+exec alacritty --class primary &;"
+sleep 0.3s;
+swaymsg "[app_id=primary] splith;"
+swaymsg "exec alacritty -o font.size=5 --class bottom -e btm --enable_gpu_memory --hide_table_gap &;"
+sleep 0.3s;
+swaymsg "[app_id=primary] resize set width 855px;"
+swaymsg "[app_id=bottom] focus; splitv;"
+swaymsg "exec alacritty -o font.size=8 --class secondary &;"
