aaa93da9344a3a463922840cf8910111baac3f2b
Alingof <Alignof@outlook.com>
2025-04-03 00:28:14 +0900

    [update] update i3 config

--- i3/script/load_layout.sh
+++ i3/script/load_layout.sh
@@ -1,4 +1,4 @@
-i3-msg "workspace 7; append_layout /home/takana/.config/i3/workspace-7.json"
+i3-msg "workspace 6; append_layout /home/takana/.config/i3/workspace-6.json"
 (alacritty -t primary &)
-(alacritty -o font.size=7 -t bottom -e btm --enable_gpu_memory --hide_table_gap &)
+(alacritty -o font.size=7 -t bottom -e btm --enable_gpu --hide_table_gap &)
 (alacritty -o font.size=10 -t secondary &)
